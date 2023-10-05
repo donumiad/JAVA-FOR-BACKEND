1709769d2f40a3cd7fb47892fe398cc476466a6e
Raimundo Neto <otenneto123@gmail.com>
2023-10-05 20:54:31 -0300

    criacao-de-package

--- primeiro projeto java sync github/src/edu/java_puro/FirstProgram.java
+++ primeiro projeto java sync github/src/edu/java_puro/FirstProgram.java
@@ -1,4 +1,5 @@
-public class App {
+package edu.java_puro;
+public class FirstProgram {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, GitHub!");
     }
