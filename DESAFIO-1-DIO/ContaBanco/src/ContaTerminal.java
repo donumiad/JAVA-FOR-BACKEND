f8273435d05bcedca53024fd1714641d78f7afa4
Raimundo Neto <otenneto123@gmail.com>
2023-10-10 21:37:30 -0300

    primeira escrita do algoritimo

--- DESAFIO-1-DIO/ContaBanco/src/ContaTerminal.java
+++ DESAFIO-1-DIO/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+    
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = 0;
+        String nomeCliente = "", agencia = "";
+        double saldo = 0;
+        Scanner dados = new Scanner(System.in);
+        
+        System.out.println("Digite o numero da agencia: ");
+        numero = dados.nextInt();
+        System.out.println("Digite a Agencia: ");
+        agencia = dados.next();
+        System.out.println("Digite seu nome: ");
+        nomeCliente = dados.next();
+        System.out.println("Valor do depósito: ");
+        saldo = dados.nextDouble();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, ", nomeCliente);
+        System.out.printf(" sua agência é %s, conta %d ",agencia, numero);
+        System.out.printf("e seu saldo %2f ja está disponível para saque",saldo);
     }
 }
